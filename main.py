91d3d9f233e99d8f91f10b6d33a86065dead6b68
Naman Arora <naarora@tcd.ie>
2020-08-04 20:00:57 +0530

    send mail again done

--- main.py
+++ main.py
@@ -36,6 +36,28 @@ def get_random_alphanumeric_string(length):
     result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
     return result_str
 
+def send_email(background_tasks: BackgroundTasks, request: Request):
+    
+    template = """
+        <html> 
+        <body>
+        <p>Hi !!!
+        <br>Thanks for using Fastapi-mail</p>
+        <p> Your passcode is : %s </p> 
+        </body> 
+        </html>
+        """ % (code)
+
+    mail = FastMail(email="your-email-here", password="your-password-here", tls=True, port="587", service="gmail")
+
+    background_tasks.add_task(mail.send_message, recipient=Email, subject="testing HTML", body=template,
+                                text_format="html")
+
+    return templates.TemplateResponse("after_email_sent_response.html",
+                        {
+                            "request": request
+                        })
+
 
 @app.get("/")
 async def get_email(request: Request):
@@ -47,33 +69,21 @@ async def get_email(request: Request):
 
 
 @app.post("/send_mail")
-async def send_mail(background_tasks: BackgroundTasks, request: Request, email: str = Form(...)):
+async def send_mail(background_tasks: BackgroundTasks, request: Request, email: Optional[str] = Form(...)):
     # this mail sending using fastapi background tasks, faster than the above one
     # Using Postman you can send post request, adding email in the body
 
-    global code
-    code = get_random_alphanumeric_string(10)
+    if email:
+        global code, Email
+        Email = email
+        code = get_random_alphanumeric_string(10)
 
-    template = """
-        <html> 
-        <body>
-        <p>Hi !!!
-        <br>Thanks for using Fastapi-mail</p>
-        <p> Your passcode is : %s </p> 
-        </body> 
-        </html>
-        """ % (code)
+    return send_email(background_tasks, request)
 
 
-    mail = FastMail(email="arora.nam21@gmail.com", password="kjol#1897", tls=True, port="587", service="gmail")
-
-    background_tasks.add_task(mail.send_message, recipient=email, subject="testing HTML", body=template,
-                                text_format="html")
-
-    return templates.TemplateResponse("after_email_sent_response.html",
-                        {
-                            "request": request
-                        })
+@app.get("/send_mail_again")
+def send_mail_again(background_tasks: BackgroundTasks, request: Request):
+    return send_email(background_tasks, request)
 
 
 @app.post("/account_recovery/")
@@ -82,14 +92,29 @@ async def verify_passcode(request: Request, passcode: Optional[str] = Form(...))
     result = ''
     if passcode == code:
         result = 'successful'
+
+        # give the result of passcode validation
+        return templates.TemplateResponse("successful_verification_result.html",
+                                {
+                                    "request": request,
+                                    "result": result
+                                })
     else:
         result = 'failed'
 
+        return templates.TemplateResponse("failed_verification.html",
+                                {
+                                    "request": request,
+                                    "result": result
+                                })
+
     # response = RedirectResponse(url='/')
-    
-    # give the result of passcode validation
-    return templates.TemplateResponse("verification_result.html",
-                            {
-                                "request": request,
-                                "result": result
-                            })
+
+
+@app.get("/re_enter_passcode")
+async def re_enter_passcode(request: Request):
+
+    return templates.TemplateResponse("after_email_sent_response.html",
+                        {
+                            "request": request
+                        })
